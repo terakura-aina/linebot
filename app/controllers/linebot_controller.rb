faf0b8b860bd910ef407a86b7f66f91f060fd486
terakura aina <aicaierwei@gmail.com>
2021-01-17 18:07:15 +0900

    rubyのバージョンを2.5.8に変更

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,37 +1,54 @@
 class LinebotController < ApplicationController
-  require 'line/bot'  # gem 'line-bot-api'
- 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
+  require 'line/bot'
+
+  protect_from_forgery :except => [:callback]
+
+  def callback
+    client = Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
-  end
- 
-  post '/callback' do
     body = request.body.read
- 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
+      head :bad_request
     end
- 
+
     events = client.parse_events_from(body)
- 
-    events.each do |event|
+
+    events.each { |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
+          if event.message['text'] == 'こんにちは'
+            client.reply_message(
+              event['replyToken'],
+              {
+                type: 'text',
+                text: 'こんにちは'
+              }
+            )
+          elsif event.message['text'] == 'こんばんは'
+            client.reply_message(
+              event['replyToken'],
+              {
+                type: 'text',
+                text: 'こんばんは'
+              }
+            )
+          else
+            client.reply_message(
+              event['replyToken'],
+              {
+                type: 'text',
+                text: 'すいません。よくわかりません。'
+              }
+            )
+          end
         end
       end
-    end
- 
-  "OK"
+    }
+    head :ok
   end
 end
