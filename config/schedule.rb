b85a5945c242d75c2e87532d541ad4d97bcf407e
terakura aina <aicaierwei@gmail.com>
2021-01-21 09:34:15 +0900

    本番環境でwheneverが動くように設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 
 rails_env = ENV['RAILS_ENV'] || :development
 
-set :environment, rails_env
+set :environment, :production
 set :output, "#{Rails.root}/log/cron.log"
 
 every 1.day, at: '10:00 am' do # タスクを処理するペースを記載する。(例は1分ごとに実行）
