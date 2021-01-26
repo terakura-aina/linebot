2c74840befb17fe45586141716a8614d824111ac
terakura aina <aicaierwei@gmail.com>
2021-01-26 10:17:54 +0900

    usersコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   post '/callback' => 'linebot#callback'
   get '/liff' => 'linebot#liff'
   get '/dateme' => 'linebot#dateme'
