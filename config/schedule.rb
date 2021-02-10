63ff07d096bffa390fdbbe5d2593a45197810bf3
terakura aina <aicaierwei@gmail.com>
2021-02-10 21:58:35 +0900

    ミッションがinviter(誘った人)に届くように実装

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,9 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 
 rails_env = ENV['RAILS_ENV'] || :development
 
-set :environment, :production
+set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
 
-every 1.day, at: ['4:00 pm'] do # タスクを処理するペースを記載する。(例は1分ごとに実行）
+every 1.day, at: ['4:00 pm'] do
   rake 'push:push_line_message'
+end
+
+every 15.minutes do
+  rake 'push:push_mission_inviter'
 end
